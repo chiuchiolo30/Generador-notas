b449e33505aa619d0abde214d92e2c888aaa5304
Edgardo <chiuchiolo30@gmail.com>
2020-06-25 22:00:56 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+   <h3>{{ titulo }}</h3> 
+    <div class="form">
+       <div class="form-group">
+         <label>Título</label>
+         <input class="form-control" type="text" v-model="nota.titulo">
+       </div>
+       <div class="form-group">        
+           <label>Texto</label>
+           <textarea class="form-control" rows="3" v-model="nota.texto"></textarea>      
+     </div>
+      <button class="btn btn-primary" @click="agregarNota">Enviar</button>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-4 nota" v-for="(nota, index) in notas" :key="nota.id">
+        <div class="card">
+          <div class="card-block">
+            <div class="card-title">{{nota.titulo}}</div>
+            <div class="card-subtitle mb-2 text-muted">{{nota.fecha}}</div>
+            <div class="card-text">{{nota.texto}}</div>
+          </div>
+          <button class="close" @click="eliminarNota(index)">&times;</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      titulo: 'Gestión de notas',
+      nota: {        
+        titulo: '',
+        texto: ''
+      },
+      notas:[
+        { 
+          id: Math.round(Math.random()*1000) ,
+          titulo: "Spiderman",
+          texto: "Viernes 25",
+          fecha: new Date(Date.now()).toLocaleString()
+          
+        }
+      ]
+    }
+  },
+  methods:{
+    agregarNota:function(){
+      let { texto, titulo} = this.nota;
+      this.notas.push({
+        id: Math.round(Math.random()*1000),
+        texto,
+        titulo,
+        fecha: new Date(Date.now()).toLocaleString()
+      });
+    },
+    eliminarNota:function(index){
+      this.notas.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style>
+  .form {
+    text-align: left;
+  }
+  .card {
+    text-align: left;
+    border: 1px solid #2c3e50;
+    border-radius: 4px;
+    padding-left: 8px;
+    padding-right: 8px;
+  }
+  .nota {
+    padding: 5px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
